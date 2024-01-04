b16a9e57a4ce8d53332cc7798104b8ebd90f305a
Benjamin Sepulveda Flores <benjamin.sepulveda@usach.cl>
2024-01-04 20:57:40 -0300

    Agrego mas cosas

---
+++ Constantes.cpp
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+
+int main(){
+
+    //Circunferencia de un circulo
+    
+    //Para hacer constante una variable se coloca la palabra reservada const antes del tipo de dato 
+
+    const double PI = 3.1416;
+
+    double radio = 10;
+
+    double circunferencia = 2 * PI * radio;
+
+    std::cout << "El valor de la circunferencia es: " << circunferencia << std::endl;
+
+
+
+
+}
